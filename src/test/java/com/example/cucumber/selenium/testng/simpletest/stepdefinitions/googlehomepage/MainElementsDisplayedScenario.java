cde9ff5558b08d40d84aeccff10d94e59fbc1823
Kay Wrobel <kay.wrobel@gmx.net>
2019-02-18 12:20:37 -0600

    Added Log4j V2 configuration and logging message to step definitions.

--- src/test/java/com/example/cucumber/selenium/testng/simpletest/stepdefinitions/googlehomepage/MainElementsDisplayedScenario.java
+++ src/test/java/com/example/cucumber/selenium/testng/simpletest/stepdefinitions/googlehomepage/MainElementsDisplayedScenario.java
@@ -1,38 +1,39 @@
 package com.example.cucumber.selenium.testng.simpletest.stepdefinitions.googlehomepage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class MainElementsDisplayedScenario {
+	
+	private static final Logger logger = LogManager.getLogger(MainElementsDisplayedScenario.class);
+	
 	@Given("I launch Chrome browser")
 	public void i_launch_Chrome_browser() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		logger.info("Browser launched");
 	}
 
 	@When("I open Google Homepage")
 	public void i_open_Google_Homepage() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		logger.info("Google Homepage opened");
 	}
 
 	@Then("I verify that the page displays search text box")
 	public void i_verify_that_the_page_displays_search_text_box() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		logger.info("Found the search text box");
 	}
 
 	@And("the page displays Google Search button")
 	public void the_page_displays_Google_Search_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		logger.info("Found the Search button");
 	}
 
 	@And("the page displays Im Feeling Lucky button")
 	public void the_page_displays_Im_Feeling_Lucky_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+		logger.info("Found the Lucky button");
 	}
 }
